80609599547cf59683ef93080ad66e3321b34736
anil-singu <anilbabu.singu@gmail.com>
2024-08-26 16:08:17 +0100

    updated headless in record_demo2

--- tests/record_demo2.spec.js
+++ tests/record_demo2.spec.js
@@ -4,7 +4,7 @@ const { chromium } = require('@playwright/test');
 
 test('record demo2',(async () => {
   const browser = await chromium.launch({
-  headless: false
+  headless: true
 });
 const context = await browser.newContext();
 const page = await context.newPage();
