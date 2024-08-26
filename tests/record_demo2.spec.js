f51af59229bb7bf4f5370c79be70b96befa1f9ac
anil-singu <anilbabu.singu@gmail.com>
2024-08-26 16:18:59 +0100

    updates to record_demo2

--- tests/record_demo2.spec.js
+++ tests/record_demo2.spec.js
@@ -13,7 +13,7 @@ await page.locator('[data-test="username"]').click();
 await page.locator('[data-test="username"]').fill('standard_user');
 await page.locator('[data-test="password"]').click();
 await page.locator('[data-test="password"]').fill('secret_sauce');
-await page.locator('[data-test="login-buttonx"]').click();
+await page.locator('[data-test="login-button"]').click();
 await page.getByRole('button', { name: 'Open Menu' }).click();
 await page.locator('[data-test="logout-sidebar-link"]').click();
 
